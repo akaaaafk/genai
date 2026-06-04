4d81ab1c2af3edf94a00c2106a14617dd1b48c3b
Shawn Sun <zs2778@columbia.edu>
2026-06-04 13:52:01 -0400

    first

--- homework/app/bigram_model.py
+++ homework/app/bigram_model.py
@@ -1,63 +1,62 @@
 import random
-from collections import defaultdict, Counter
+import re
+from collections import Counter, defaultdict
 
 
 class BigramModel:
-    def __init__(self, corpus):
+    def __init__(self, corpus, frequency_threshold=None):
         self.corpus = corpus
+        self.frequency_threshold = frequency_threshold
+        self.vocab = []
         self.bigram_probs = defaultdict(dict)
         self.train()
 
     def tokenize(self, text):
-        """
-        简单分词：
-        1. 转小写
-        2. 按空格切分
-        """
-        return text.lower().split()
+        tokens = re.findall(r"\b\w+\b", text.lower())
+
+        if not self.frequency_threshold:
+            return tokens
+
+        word_counts = Counter(tokens)
+
+        return [
+            token for token in tokens
+            if word_counts[token] >= self.frequency_threshold
+        ]
 
     def train(self):
-        """
-        训练 bigram model：
-        统计每个词后面最可能出现哪些词。
-        """
-        bigram_counts = defaultdict(Counter)
+        if isinstance(self.corpus, list):
+            text = " ".join(self.corpus)
+        else:
+            text = self.corpus
 
-        for sentence in self.corpus:
-            words = self.tokenize(sentence)
+        words = self.tokenize(text)
 
-            # 生成 bigrams，比如 ["i", "love", "you"]
-            # 会变成 ("i", "love"), ("love", "you")
-            for w1, w2 in zip(words[:-1], words[1:]):
-                bigram_counts[w1][w2] += 1
+        unigram_counts = Counter(words)
+        bigrams = list(zip(words[:-1], words[1:]))
+        bigram_counts = Counter(bigrams)
 
-        # 把 count 转成 probability
-        for w1, next_words in bigram_counts.items():
-            total = sum(next_words.values())
+        self.vocab = list(unigram_counts.keys())
 
-            for w2, count in next_words.items():
-                self.bigram_probs[w1][w2] = count / total
+        for (word1, word2), count in bigram_counts.items():
+            self.bigram_probs[word1][word2] = count / unigram_counts[word1]
 
     def generate_next_word(self, current_word):
-        """
-        根据当前词，生成下一个词。
-        """
         current_word = current_word.lower()
+        next_words = self.bigram_probs.get(current_word)
 
-        if current_word not in self.bigram_probs:
+        if not next_words:
             return None
 
-        next_words = list(self.bigram_probs[current_word].keys())
-        probabilities = list(self.bigram_probs[current_word].values())
+        return random.choices(
+            list(next_words.keys()),
+            weights=list(next_words.values()),
+            k=1
+        )[0]
 
-        return random.choices(next_words, weights=probabilities, k=1)[0]
-
-    def generate_text(self, start_word, length):
-        """
-        从 start_word 开始生成 length 个词。
-        """
-        words = [start_word]
+    def generate_text(self, start_word, length=20):
         current_word = start_word.lower()
+        generated_words = [current_word]
 
         for _ in range(length - 1):
             next_word = self.generate_next_word(current_word)
@@ -65,7 +64,27 @@ class BigramModel:
             if next_word is None:
                 break
 
-            words.append(next_word)
+            generated_words.append(next_word)
             current_word = next_word
 
-        return " ".join(words)
+        return " ".join(generated_words)
+
+    def get_bigram_probs(self):
+        return {
+            word1: dict(next_words)
+            for word1, next_words in self.bigram_probs.items()
+        }
+
+    def get_vocab(self):
+        return self.vocab
+
+    def get_bigram_matrix(self):
+        matrix = {}
+
+        for word1 in self.vocab:
+            matrix[word1] = {}
+
+            for word2 in self.vocab:
+                matrix[word1][word2] = self.bigram_probs.get(word1, {}).get(word2, 0)
+
+        return matrix
